fbdc8ade074e521d060c4aca9796bbb370d71425
Sam Driver <sdriver@gmx.co.uk>
2023-05-20 22:48:01 +0100

    self-host font

--- app/includes/Head.php
+++ app/includes/Head.php
@@ -13,7 +13,6 @@ class Head
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto&family=Roboto+Mono&display=swap">
             <link rel="stylesheet" href="/css/style.css">
 
             <!-- Favicon -->
@@ -31,6 +30,34 @@ class Head
             <meta name="description" content="{$description}"/>
             <meta name="author" content="Sam Driver">
             <title>{$title}</title>
+
+            <style>
+            @font-face {
+                font-display: swap;
+                font-family: 'Roboto';
+                font-style: normal;
+                font-weight: 400;
+                src: url('/fonts/roboto-v30-latin-regular.eot'); /* IE9 Compat Modes */
+                src: url('/fonts/roboto-v30-latin-regular.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+                    url('/fonts/roboto-v30-latin-regular.woff2') format('woff2'), /* Super Modern Browsers */
+                    url('/fonts/roboto-v30-latin-regular.woff') format('woff'), /* Modern Browsers */
+                    url('/fonts/roboto-v30-latin-regular.ttf') format('truetype'), /* Safari, Android, iOS */
+                    url('/fonts/roboto-v30-latin-regular.svg#Roboto') format('svg'); /* Legacy iOS */
+            }
+            /* roboto-700 - latin */
+            @font-face {
+                font-display: swap;
+                font-family: 'Roboto';
+                font-style: normal;
+                font-weight: 700;
+                src: url('/fonts/roboto-v30-latin-700.eot'); /* IE9 Compat Modes */
+                src: url('/fonts/roboto-v30-latin-700.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+                    url('/fonts/roboto-v30-latin-700.woff2') format('woff2'), /* Super Modern Browsers */
+                    url('/fonts/roboto-v30-latin-700.woff') format('woff'), /* Modern Browsers */
+                    url('/fonts/roboto-v30-latin-700.ttf') format('truetype'), /* Safari, Android, iOS */
+                    url('/fonts/roboto-v30-latin-700.svg#Roboto') format('svg'); /* Legacy iOS */
+            }
+            </style>
         </head>
         EOD;
     }
